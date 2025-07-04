e22cd2be9c36f1f39f2f072adb07aaf5243ce052
georgebush2021 <rishabh.rakesh.tandon@gmail.com>
2025-07-04 20:41:19 +0530

    Add files via upload

---
+++ api/generate-pdf.js
@@ -0,0 +1,41 @@
+
+const puppeteer = require('puppeteer');
+const ejs = require('ejs');
+const fs = require('fs');
+const path = require('path');
+
+module.exports = async (req, res) => {
+  if (req.method !== 'POST') return res.status(405).send('Method Not Allowed');
+
+  const payload = req.body;
+
+  try {
+    const templatePath = path.join(__dirname, '..', 'template.ejs');
+    const template = fs.readFileSync(templatePath, 'utf-8');
+
+    const html = ejs.render(template, {
+      ...payload,
+      successDriversLogo: 'data:image/png;base64,' + fs.readFileSync(path.join(__dirname, '..', 'success_drivers.png')).toString('base64'),
+      actLogo: 'data:image/png;base64,' + fs.readFileSync(path.join(__dirname, '..', 'act.png')).toString('base64'),
+      chartScript: '',
+      radarChartScript: ''
+    });
+
+    const browser = await puppeteer.launch({
+      args: ['--no-sandbox']
+    });
+
+    const page = await browser.newPage();
+    await page.setContent(html, { waitUntil: 'networkidle0' });
+
+    const pdfBuffer = await page.pdf({ format: 'A4', printBackground: true });
+    await browser.close();
+
+    res.setHeader('Content-Type', 'application/pdf');
+    res.setHeader('Content-Disposition', 'inline; filename=report.pdf');
+    res.end(pdfBuffer);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('PDF generation failed.');
+  }
+};
